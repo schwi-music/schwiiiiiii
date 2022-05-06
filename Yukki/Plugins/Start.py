d90cebc3c4bc2e94aabf3bf5372e627158cb1fd2
Lord dsp <87901742+Lord-DSP3@users.noreply.github.com>
2022-05-06 14:33:39 +0530

    Update Start.py

--- Yukki/Plugins/Start.py
+++ Yukki/Plugins/Start.py
@@ -85,7 +85,7 @@ async def useradd(_, message: Message):
     await asyncio.gather(
         message.delete(),
         message.reply_text(
-            f"https://telegra.ph/file/466b79ea3804ce33e731d.mp4          💕🎋Thanks for having me in {message.chat.title}.\n{MUSIC_BOT_NAME} is alive.\n\n Owner -𝕃𝕠𝕣𝕕_𝔻𝕊ℙ_𝟛.",
+            f"https://telegra.ph/file/466b79ea3804ce33e731d.mp4               💕🎋Thanks for having me in {message.chat.title}.\n{MUSIC_BOT_NAME} is alive.\n\n Owner -𝕃𝕠𝕣𝕕_𝔻𝕊ℙ_𝟛.",
             reply_markup=InlineKeyboardMarkup(out[1]),
         ),
     )
@@ -119,7 +119,7 @@ async def okaybhai(_, CallbackQuery):
     await CallbackQuery.answer("Going Back ...")
     out = start_pannel()
     await CallbackQuery.edit_message_text(
-        text=f"https://telegra.ph/file/466b79ea3804ce33e731d.mp4 Thanks for having me in {CallbackQuery.message.chat.title}.\n{MUSIC_BOT_NAME}is alive.\n\nFor any assistance or help, Made by -𝕃𝕠𝕣𝕕_𝔻𝕊ℙ_𝟛.",
+        text=f"https://telegra.ph/file/466b79ea3804ce33e731d.mp4                 💕🎋Thanks for having me in {CallbackQuery.message.chat.title}.\n{MUSIC_BOT_NAME}is alive.\n\nFor Owner- 𝕃𝕠𝕣𝕕_𝔻𝕊ℙ_𝟛.",
         reply_markup=InlineKeyboardMarkup(out[1]),
     )
 
